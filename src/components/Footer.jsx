7632355418fc3b30d12be6bb99be0b9639ddd7aa
Ivan Caldarella <gamein3x@gmail.com>
2026-06-11 16:08:07 +0200

    Aggiunto contenuto template a Header e Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-
-
 function Footer() {
     return (
         <footer>
-
+            <div class="footer-info">
+                <p><strong>Il Pork-Edotto (C)</strong></p>
+                <p>Via Pippo 67 - Piano 15 (Settore Orbitale)</p>
+            </div>
+            <div class="footer-circle"></div>
         </footer>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 function Header() {
     return (
         <header>
-
+            <div class="logo-container">
+                <h1>IL PORK <div class="logo-circle"></div> EDOTTO</h1>
+            </div>
+            <nav>
+                <a href="#home">HOME</a>
+                <a href="#menu">MENU</a>
+                <a href="#reviews">PEER-REVIEWS</a>
+                <a href="#contatti">CONTATTI</a>
+            </nav>
         </header>
     );
 }
