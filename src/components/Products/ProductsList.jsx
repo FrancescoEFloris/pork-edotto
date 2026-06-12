55950b8bb3640c331a20e020c6ba42baebbc60a3
Francesco Cassese <francescocassese9@gmail.com>
2026-06-12 16:48:55 +0200

    refactor: update ProductList and ProductDetail to use generic useFetch hook

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,50 +1,36 @@
-import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import fetchProducts from '../../handlers/fetchHandler.js';
 import ProductDetail from './ProductDetail.jsx';
-import styles from './ProductsList.module.css'
+import styles from './ProductsList.module.css';
+import useFetch from '../../hooks/useFetch.js';
 
 
 function ProductsList() {
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState(null);
+    const { data: products, loading, error } = useFetch('/products');
 
-    useEffect(() => {
-        async function loadData() {
-            try {
-                const dataResponse = await fetchProducts();
-                setProducts(dataResponse.data);
-            } catch (error) {
-                setError("Could not load the products");
-            }
-        }
-        loadData();
-    }, []);
+    if (error) return <p>Errore: {error}</p>;
+
+
+    if (loading || !products) {
+        return <main><p>Caricamento prodotti...</p></main>;
+    }
     // console.log(products);
     if (error) return <p>Errore: {error}</p>;
 
-    const wrapper = (
-        <div className="container">
-            <div className="row row-cols-2">
-                {products.map((thisProduct) => (
-                    <div key={thisProduct.id} className={`${styles.productCard}`}>
-                        <h3>{thisProduct.name}</h3>
-                        <p>{thisProduct.description}</p>
-                        <Link to={`/products/${thisProduct.id}`} >
-                            <img src={thisProduct.image} alt={thisProduct.title} className='product-img' />
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-
     return (
         <main>
-            <div className="">
-                {products.length === 0 ? (
-                    <p>Caricamento prodotti</p>
-                ) : wrapper}
+            <div className="container">
+                <div className="row row-cols-2">
+
+                    {products.map((thisProduct) => (
+                        <div key={thisProduct.id} className={`${styles.productCard}`}>
+                            <h3>{thisProduct.name}</h3>
+                            <p>{thisProduct.description}</p>
+                            <Link to={`/products/${thisProduct.id}`} >
+                                <img src={thisProduct.image} alt={thisProduct.title} className='product-img' />
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
         </main>
     );
