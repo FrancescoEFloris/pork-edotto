fdd4e73bde3652a30dc83d9772adca7d800c1228
Ivan Caldarella <gamein3x@gmail.com>
2026-06-12 06:23:33 +0200

    Progressi con header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <header className={`${styles.header}`}>
-            <div className="container-fluid py-2">
+            <div className={`container-fluid py-2 ${styles.headContainer}`}>
                 <div className="row align-items-center">
 
                     {/* LOGO: Al lato (sinistra). Più grande su md/lg, leggermente ridotto in sm */}
-                    <div className="col-4 col-md-3 col-lg-2">
+                    <div className="col-4 col-md-3 col-lg-3">
                         <div className={`${styles.logoContainer}`}>
                             <div className={`${styles.imgContainer}`}>
                                 <img
@@ -20,8 +20,8 @@ function Header() {
                     </div>
 
                     {/* NAVBAR: Allineata a sinistra con layout fluido dei NavItems */}
-                    <div className="col-8 col-md-9 col-lg-10">
-                        <ul className="row row-cols-2 row-cols-lg-4 list-unstyled m-0 p-0 text-start align-items-center py-lg-5 py-sm-4">
+                    <div className={`col-8 col-md-9 col-lg-9 ${styles.navPages}`}>
+                        <ul className="row row-cols-2 row-cols-lg-4 list-unstyled m-0 p-0 text-center align-items-center py-lg-5 py-sm-4">
                             <li className="col nav-item mb-2 mb-lg-0">
                                 <a className="nav-link btn-primary d-inline-block" href="/">Home</a>
                             </li>
