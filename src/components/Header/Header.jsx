8d0286dfa5d6ea31a536ceee27ca8b8fdadec086
Ivan Caldarella <gamein3x@gmail.com>
2026-06-12 04:56:43 +0200

    Aggiunta immagine logo in Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,25 +5,27 @@ function Header() {
         <header className={`${styles.header}`}>
 
             <div className={`${styles.logoContainer}`}>
-                <h1>IL PORK - EDOTTO</h1>
+                <div className={`${styles.imgContainer}`}>
+                    <img src="src\public\images\porkedotto-logo.png" alt="Logo principale" className="img-fluid"/>
+                </div>
             </div>
             <nav className="">
 
                 <div className="container-fluid">
-                        <ul className="d-flex justify-content-around">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Menù</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Peer-Reviews</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Chi siamo</a>
-                            </li>
-                        </ul>
+                    <ul className="d-flex justify-content-around">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Menù</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Peer-Reviews</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Chi siamo</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
