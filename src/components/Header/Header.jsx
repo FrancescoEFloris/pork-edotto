6004e6c06efeeafb56039d24c8b8e1ce6a544697
rAel-Ael <alediru9267@gmail.com>
2026-06-12 17:55:23 +0200

    fixato bordino bianco

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,8 +14,7 @@ function Header() {
     return (
         <header className={`${styles.header}`}>
             <div className={`container-fluid ${styles.headContainer}`}>
-                <div className="row align-items-center position-relative">
-
+                <div className="row align-items-center position-relative container-fluid">
                     <div className="col-6 col-xl-3">
                         <div className={`${styles.logoContainer}`}>
                             <div className={`${styles.imgContainer}`}>
@@ -27,16 +26,13 @@ function Header() {
                             </div>
                         </div>
                     </div>
-
                     <div className="col-6 col-xl-9 d-flex justify-content-end justify-content-xl-start align-items-center">
-
                         <ul className={`list-unstyled m-0 p-0 text-center align-items-center ${styles.navDesktop}`}>
                             <li><NavLink className={getNavLinkClass} to="/">Home</NavLink></li>
                             <li><NavLink className={getNavLinkClass} to="/products">Menu</NavLink></li>
                             <li><NavLink className={getNavLinkClass} to="/pippo">Peer-Reviews</NavLink></li>
                             <li><NavLink className={getNavLinkClass} to="/aboutus">Chi siamo</NavLink></li>
                         </ul>
-
                         <button
                             className={`${styles.hamburger} ${isOpen ? styles.open : ''}`}
                             onClick={toggleMenu}
@@ -46,7 +42,6 @@ function Header() {
                             <span className={styles.bar}></span>
                             <span className={styles.bar}></span>
                         </button>
-
                         <ul className={`${styles.navMobile} ${isOpen ? styles.active : ''} list-unstyled m-0 p-0 text-center`}>
                             <li><NavLink className={getNavLinkClass} to="/" onClick={toggleMenu}>Home</NavLink></li>
                             <li><NavLink className={getNavLinkClass} to="/products" onClick={toggleMenu}>Menu</NavLink></li>
