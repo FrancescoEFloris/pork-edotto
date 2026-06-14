24a1903c2d4853d27cc05c2eeb31d1cb596645b9
giulia-buonasperanza <giulia.9402@gmail.com>
2026-06-14 19:01:08 +0200

    modifica RenderStar per avere le stelle a metà

--- src/components/Reviews/RenderStar.jsx
+++ src/components/Reviews/RenderStar.jsx
@@ -1,11 +1,17 @@
 function RenderStars(rating = 0) {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating - fullStars >= 0.5;
     const stars = [];
 
     for (let i = 0; i < 5; i++) {
-        stars.push(
-            <i key={i}
-                className={i < rating ? "bi bi-star-fill text-warning" : "bi bi-star text-warning"} />
-        );
+        if (i < fullStars) {
+            stars.push(<i key={i} className="bi bi-star-fill text-warning" />);
+        } else if (i === fullStars && halfStar) {
+            stars.push(<i key={i} className="bi bi-star-half text-warning" />);
+        }
+        else {
+            stars.push(<i key={i} className="bi bi-star text-warning" />);
+        }
     }
 
     return stars;
