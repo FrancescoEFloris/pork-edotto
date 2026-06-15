d4d14ab20cedd4055d10904f91665697552136c4
Francesco Cassese <francescocassese9@gmail.com>
2026-06-15 15:17:15 +0200

    feat/ai-predefined-questions

---
+++ src/data/questionsData.js
@@ -0,0 +1,24 @@
+const predefinedQuestions = [
+    {
+        id: 1,
+        label: " Ansia da esame orale",
+        text: "Ho un esame orale e sono tesissimo, che panino mi consigli?"
+    },
+    {
+        id: 2,
+        label: " Esame a crocette rapido",
+        text: "Ho un esame a crocette veloce, cosa mangio al volo senza perdere tempo?"
+    },
+    {
+        id: 3,
+        label: "Le tasse universitarie mi hanno già spennato e adesso sono povero",
+        text: "Ho pagato così tante tasse che il mio portafoglio fa eco, qual'è il prodotto che costa meno?"
+    },
+    {
+        id: 4,
+        label: "Cosa posso fare se sono vegano?",
+        text: "Sono vegano, hai qualche consiglio da darmi?"
+    }
+]
+
+export default predefinedQuestions;
